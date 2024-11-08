f70720170eb515c357c89e7d54c005089b8c311c
daminisherathiya <daminisherathiya@gmail.com>
2024-11-08 16:21:45 +0530

    Bug fixes in the property listing flow (#5)

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,3 +4,7 @@ export function removeLeadingZeros(value: string) {
   const removeLeadingZeros = value.replace(/^0+/, "");
   return removeLeadingZeros === "" ? "0" : removeLeadingZeros;
 }
+
+export const roundNumber = (value: number): string => {
+  return value % 1 === 0 ? value.toString() : value.toFixed(2);
+};
