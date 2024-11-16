d147e86db2e8f891fbe029a89e7e2d803a9d5463
daminisherathiya <daminisherathiya@gmail.com>
2024-11-16 18:42:07 +0530

    Fix the issue of some Tailwind CSS classes not working

--- src/theme/tailwind.config.ts
+++ src/theme/tailwind.config.ts
@@ -5,9 +5,7 @@ import { palette } from "./colors";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   important: "#html",
   plugins: [],
